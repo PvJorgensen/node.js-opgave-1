8cc9657e2fcff0caf8cd2b6a4f9925ac2d6e2203
Patrik_Jørgensen <vadmand806@gmail.com>
2023-11-23 13:49:32 +0100

    Update

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-import http from 'http';
+import express from "express";
 
-http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('Hello World!');
-    response.end();
-}).listen(4000)
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Velkommen til min side');
+})
+
+app.get('/about', (req, res) => {
+    console.log(req.query);
+    res.send('Om os');
+})
+
+app.listen(4000,() => {
+
+    console.log('Nu køre serveren med express');
+})
